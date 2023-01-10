da7506c0a7dc9198a187a3d219f629c0c1bcff99
koulako <koulakokourouma31@gmail.com>
2023-01-10 08:42:55 -0800

    KK

--- script.js
+++ script.js
@@ -1,232 +1,259 @@
-//Declaration des données du tableau 
-const donneDepRev =(function (){
-    const Item= function(id,title, amount){
-      this.id=id;
-      this.title=title;
-      this.amount=amount;
-    };
-    
-    //definition du tableau
-    const data={
-      items: [],
-    };
-    return {
-      dataItem:function(){
-        return data;
-      },
-      ajouter: function(title,amount){
-        let ID = donneDepRev.createID();
-        //creation d'un nouveau
-        newAjouter = new Item(ID,title,amount);
-       //pouser dans le tableau
-        data.items.push(newAjouter);
-       //retour
-        return newAjouter;
-      },
-      createID: function () {
-        //créer au hasard des nombres compris entre 0 et 10000 pour les id
-        const idNum = Math.floor(Math.random() * 10000);
-        return idNum;
-      },
-      
-    }
-  })();
-  // controler les donnees
-  const executDepRev=(function(){
-    //declaration des selecteurs
-    const Selecteur={
-      title:"#title",
-      amount:"#amount",
-      ajouBudget:"#budget_fcfa",
-      ajouDepense:"#depense_fcfa",
-      ajouSolde:"#solde_fcfa",
-      itemrevenu:".item_revenu",
-      itemdepense:".item_depense",
-      revenu:"#list_revenu",
-      depense:"#list_depense",
-      btnrevenu:"#btn_ajout_rev",
-      btndepense:"#btn_ajout_dep",
-      deleteListe:".sup",
-      updateListe:".modif",
-    };
-    return {
-      //recuperation des inputs au niveau du css
-      repSelecteur: function (){
-        return Selecteur;
-      },
-      repTitle: function() {
+const itemCtrl = (function () {
+  const Item = function (id, title, amount) {
+    this.id = id;
+    this.title = title;
+    this.amount = amount;
+  };
+
+  const data = {
+    items: [],
+  };
+  return {
+    logData: function () {
+      return data;
+    },
+    addMoney: function (title, amount) {
+      //créer un id au hasard
+      let ID = itemCtrl.createID();
+      //créer new item
+      newMoney = new Item(ID, title, amount);
+      //poussez le dans le tableau
+      data.items.push(newMoney);
+
+      return newMoney;
+    },
+    createID: function () {
+      //créer au hasard des nombres compris entre 0 et 10000 pour les id
+      const idNum = Math.floor(Math.random() * 10000);
+      return idNum;
+    },
+    getIdNumber: function (item) {
+      //get the item id
+      const amountId = item.parentElement.id;
+      //break the id into an array
+      const itemArr = amountId.split("-");
+      //get the id number
+      const id = parseInt(itemArr[1]);
+
+      return id;
+    },
+    deleteAmountArr: function (id) {
+      //get all the ids
+      const ids = data.items.map(function (item) {
+        return item.id;
+      });
+
+      const index = ids.indexOf(id);
+      data.items.splice(index, 1);
+    },
+  };
+})();
+
+const UICtrl = (function () {
+  const UISelectors = {
+    incomeBtn: "#btn_ajout_rev",
+    expenseBtn: "#btn_ajout_dep",
+    description: "#title",
+    amount: "#amount",
+    moneyEarned: "#budget_fcfa",
+    moneyAvailable: "#solde_fcfa",
+    moneySpent: "#depense_fcfa",
+    incomeList: "#list_revenu",
+    expensesList: "#list_depense",
+    incomeItem: ".income__amount",
+    expenseItem: ".expense__amount",
+    itemsContainer: ".liste_depensedeprev",
+  };
+
+  return {
+    getSelectors: function () {
+      return UISelectors;
+    },
+    getDescriptionInput: function () {
       return {
-        titleInput: document.querySelector(Selecteur.title).value,
+        descriptionInput: document.querySelector(UISelectors.description).value,
       };
-      },
-      repAmount: function() {
-        return {
-          amountInput: document.querySelector(Selecteur.amount).value,
-        };
-      },
-      //creation,recuperation, modification et suppresion  revevenu
-      //creer la liste des revenus
-      recRevenu: function(item){
-        //creer une section
-        const div=document.createElement("div");
-        //creer sa class
-        div.classList="itemrevenu";
-        div.id = `item-${item.id}`;
-        div.innerHTML= `<h3>${item.title}</h3>
-        <div class="item_revenu1">
-          <span class="item_revenu">${item.amount}</span>
-          <p class="symbol">F CFA</p>
-        </div>
-        <p><a class="sup" href="">Suprimer</a></p>
-        `; 
-        document.querySelector(Selecteur.revenu).insertAdjacentElement("beforeend",div);
-      },
-      clearInputs: function () {
-        document.querySelector(Selecteur.title).value = "";
-        document.querySelector(Selecteur.amount).value = "";
-      },
-      //metre à jour et afficher le budget
-      miseBudget: function (){
-        //recuperation de tous les revenus
-        const bugetTotal= document.querySelectorAll(Selecteur.itemrevenu);
-       //parcours du tableau
-        const budgetCompte= [...bugetTotal].map((item)=> +item.innerHTML);
-       //calcule du budget nouveau
-        const budSomme= budgetCompte.reduce(function (a , b){
-          return a + b;
-        }, 0);
-        //afficher le budget total
-        const afficheRevenu=(document.querySelector(Selecteur.ajouBudget).innerHTML=budSomme.toFixed(0));
-      },
-      //creation,recuperation, modification et suppresion  revevenu
-      //creer la liste des depenses
-      recDepense: function(item){
-        const div=document.createElement("div");
-        //creer une class
-        div.classList="itemdepense";
-        div.id = `item-${item.id}`;
-        div.innerHTML= `<h3>${item.title}</h3>
-        <div class="item_depense1">
-          <span class="item_depense">${item.amount}</span>
-          <p class="symbol">F CFA</p>
-        </div>
-        <p> <a class="sup" href="">Suprimer</a></p>
-        `;
-        document.querySelector(Selecteur.depense).insertAdjacentElement("beforeend", div)
-      },
-      //metre à jour et afficher depense
-      miseDepense: function (){
-        //recuperation de tous les revenus
-        const depenseTotal= document.querySelectorAll(Selecteur.itemdepense);
-       //parcours du tableau
-        const depenseCompte= [...depenseTotal].map((item)=> +item.innerHTML);
-       //calcule du budget
-        const depenseSomme= depenseCompte.reduce(function (a , b){
-          return a + b;
-        }, 0);
-        //afficher le budget total
-        const affiche2=(document.querySelector(Selecteur.ajouDepense).innerHTML=depenseSomme);
-      },
-      //metre à jour et afficher le solde
-      miseSolde: function(){
-        //recuperation de revenu depense et solde
-        const revTotal=document.querySelector(Selecteur.ajouBudget);
-        const depTotal=document.querySelector(Selecteur.ajouDepense);
-        const solTotal=document.querySelector(Selecteur.ajouSolde);
-        //afficher le solde total
-        solTotal.innerHTML=(+revTotal.innerHTML - +depTotal.innerHTML).toFixed(0);
-      },
+    },
+    getValueInput: function () {
+      return {
+        amountInput: document.querySelector(UISelectors.amount).value,
+      };
+    },
+    addIncomeItem: function (item) {
+      //Créer une nouvelle section
+      const div = document.createElement("div");
+      //ajouter une classe
+      div.classList = "itemrevenu";
+      //ajouter un id dans item
+      div.id = `item-${item.id}`;
+      //ajouter du html
+      div.innerHTML = `
+            <h4>${item.title}</h4>
+            <div class="item__income">
+              <span class="income__amount">${item.amount}</span>
+              <p class="symbol">FCFA</p>
+            </div>
+            <i class="fa-solid fa-trash-can"></i>
+            `;
+      //insérer des revenus dans list
+      document
+        .querySelector(UISelectors.incomeList)
+        .insertAdjacentElement("beforeend", div);
+    },
+    clearInputs: function () {
+      document.querySelector(UISelectors.description).value = "";
+      document.querySelector(UISelectors.amount).value = "";
+    },
+    updateEarned: function () {
+      //tous les éléments revenu
+      const allIncome = document.querySelectorAll(UISelectors.incomeItem);
+      //array with all incomes
+      const incomeCount = [...allIncome].map((item) => +item.innerHTML);
+      //calculer le budget total
+      const incomeSum = incomeCount.reduce(function (a, b) {
+        return a + b;
+      }, 0);
+      //affichage du budget total
+      const earnedTotal = (document.querySelector(
+        UISelectors.moneyEarned
+      ).innerHTML = incomeSum.toFixed(0));
+    },
+    addExpenseItem: function (item) {
+      //créer une nouvelle section
+      const div = document.createElement("div");
+      //ajouter une classe
+      div.classList = "itemdepense";
+      //ajouter un id dans item
+      div.id = `item-${item.id}`;
+      //ajouter du  html
+      div.innerHTML = `
+            <h4>${item.title}</h4>
+            <div class="item__expense">
+                <span class="expense__amount">${item.amount}</span>
+                <p class="symbol">FCFA</p>
+            </div>
+            <i class="fa-solid fa-trash-can"> </i>
+            `;
+      //insérer des dépenses dans la liste
+      document
+        .querySelector(UISelectors.expensesList)
+        .insertAdjacentElement("beforeend", div);
+    },
+    updateSpent: function () {
+      //tous les éléments dépenses
+      const allExpenses = document.querySelectorAll(UISelectors.expenseItem);
+      //tableau de toutes les dépenses
+      const expenseCount = [...allExpenses].map((item) => +item.innerHTML);
+      //calculer le total des dépenses
+      const expenseSum = expenseCount.reduce(function (a, b) {
+        return a + b;
+      }, 0);
+      // Afficher les dépenses totales
+      const expensesTotal = (document.querySelector(
+        UISelectors.moneySpent
+      ).innerHTML = expenseSum);
+    },
+    updateAvailable: function () {
+      const earned = document.querySelector(UISelectors.moneyEarned);
+      const spent = document.querySelector(UISelectors.moneySpent);
+      const available = document.querySelector(UISelectors.moneyAvailable);
+      available.innerHTML = (+earned.innerHTML - +spent.innerHTML).toFixed(0);
+    },
+    deleteAmount: function (id) {
+      //create the id we will select
+      const amountId = `#item-${id}`;
+      //select the amount with the id we passed
+      const amountDelete = document.querySelector(amountId);
+      //remove from ui
+      amountDelete.remove();
+    },
+  };
+})();
+
+const App = (function () {
+  //event listeners
+  const loadEventListeners = function () {
+    //get ui selectors
+    const UISelectors = UICtrl.getSelectors();
+    //ajouter un nouveau revenu
+    document
+      .querySelector(UISelectors.incomeBtn)
+      .addEventListener("click", addIncome);
+    //ajouter une nouvelle dépense
+    document
+      .querySelector(UISelectors.expenseBtn)
+      .addEventListener("click", addExpense);
+
+    document
+      .querySelector(UISelectors.itemsContainer)
+      .addEventListener("click", deleteItem);
+  };
+
+  //ajouter revenu
+  const addIncome = function () {
+    //obtenir la description et la valeur du montant
+    const description = UICtrl.getDescriptionInput();
+    const amount = UICtrl.getValueInput();
+    //si les entrées ne sont pas vides
+    if (description.descriptionInput !== "" && amount.amountInput !== "") {
+      const newMoney = itemCtrl.addMoney(
+        description.descriptionInput,
+        amount.amountInput
+      );
+      //add item to the list
+      UICtrl.addIncomeItem(newMoney);
+      //effacer les entrées
+      UICtrl.clearInputs();
+      //mettre à jour le budget
+      UICtrl.updateEarned();
+      //calculer le solde
+      UICtrl.updateAvailable();
     }
-  })();
-  //Execution App
-  const App= (function(){
-    //Evenement
-    const Ajouterlistener =function(){
-      //Recuperation du selecteur global
-      const RecSelecteur=executDepRev.repSelecteur();
-      //Ajouter Revenus
-      document.querySelector(RecSelecteur.btnrevenu).addEventListener("click", ajouterRev);
-      //Ajouter depense
-      document.querySelector(RecSelecteur.btndepense).addEventListener("click", ajouterDep);
-      //Suprimer
-      document.querySelectorAll(RecSelecteur.deleteListe).forEach(b => {
-        b.addEventListener("click", function() {
-          return deleteItem(this.value);
-        });
-      });
-      //Modification
-      document.querySelectorAll(RecSelecteur.updateListe).forEach(b => {
-        b.addEventListener("click", function() {
-          return modifItem(this.value);
-        });
-      });
-    };
-    const ajouterRev =function(){
-      //recuperation le title et amount
-      const title= executDepRev.repTitle();
-      const amount= executDepRev.repAmount();
-      
-      if(title.titleInput !=="" && amount.amountInput !==""){
-        const newAjouter= donneDepRev.ajouter(title.titleInput, amount.amountInput);
-        //ajouter dans la list
-        executDepRev.recRevenu(newAjouter);
-        //clear
-        executDepRev.clearInputs(); 
-        //nouveau budget
-        executDepRev.miseBudget();
-        //nouveau solde
-        executDepRev.miseSolde();  
-      }else if(title.titleInput ===''){
-        alert("Veuilez Saisir le Titre!!!");
-      }else if(amount.amountInput==''){
-        alert("Veuilez Saisir le montant!!!")
-      }
-    };
-    const ajouterDep=function(){
-      const title= executDepRev.repTitle();
-      const amount= executDepRev.repAmount();
-      
-      if(title.titleInput !=="" && amount.amountInput !==""){
-        const newAjouter= donneDepRev.ajouter(title.titleInput, amount.amountInput);
-        //ajouter dans la list
-        executDepRev.recDepense(newAjouter);
-        //clear
-        executDepRev.clearInputs(); 
-        //nouveau depense
-        executDepRev.miseDepense();  
-        //nouveau solde
-        executDepRev.miseSolde();
-      }else if(title.titleInput ===''){
-        alert("Veuilez Saisir le Titre!!!");
-      }else if(amount.amountInput==''){
-        alert("Veuilez Saisir le montant!!!")
-      }
-    };
-    const deleteItem = function (index) {
-      if (confirm("Confirmez-vous la suppression ?")) {
-        data.items.splice(index, 1);
-        //mettre à jour le budget
-        executDepRev.miseBudget();
-        //mettre à jour les dépenses
-        executDepRev.miseDepense();
-        //calculer le solde
-        executDepRev.miseSolde();
-      }
-    };
-    // const modifItem= function (index) {
-    //   // Récupération de la ligne via son index
-    //   const rec = dataItem.find((m, i) => {
-    //     return i == index;
-    //   });
-    
-    //   // Alimentation des champs
-    //   document.getElementById("title").value = rec.title;
-    //   document.getElementById("amount").value = rec.amount;
-    // };
-    return {
-      init: function () {
-        Ajouterlistener();
-      },
-    };
-  })(donneDepRev, executDepRev);
-  App.init();
+  };
+
+  //ajouter dépense
+  const addExpense = function () {
+    //obtenir la description et la valeur du montant
+    const description = UICtrl.getDescriptionInput();
+    const amount = UICtrl.getValueInput();
+    //si les entrées ne sont pas vides
+    if (description.descriptionInput !== "" && amount.amountInput !== "") {
+      const newMoney = itemCtrl.addMoney(
+        description.descriptionInput,
+        amount.amountInput
+      );
+      UICtrl.addExpenseItem(newMoney);
+      //effacer les entrées
+      UICtrl.clearInputs();
+      //mettre à jour les dépenses
+      UICtrl.updateSpent();
+      //calculer le solde
+      UICtrl.updateAvailable();
+    }
+  };
+
+  const deleteItem = function (e) {
+    if (e.target.classList.contains("fa-solid")) {
+      const id = itemCtrl.getIdNumber(e.target);
+      UICtrl.deleteAmount(id);
+      itemCtrl.deleteAmountArr(id);
+      //mettre à jour le budget
+      UICtrl.updateEarned();
+      //mettre à jour les dépenses
+      UICtrl.updateSpent();
+      //calculer le solde
+      UICtrl.updateAvailable();
+    }
+
+    e.preventDefault();
+  };
+
+  //initialisation des fonctions
+  return {
+    init: function () {
+      loadEventListeners();
+    },
+  };
+})(itemCtrl, UICtrl);
+
+App.init();
